f0b7ed11c7297ddb4d41ba4f37da96452caece1d
Jérôme BRASSEUR <raxiocool@gmail.com>
2025-07-19 11:55:44 +0200

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"  # Version récente compatible avec les modules EKS modernes
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.9"
+    }
+  }
+}
